b976807d87f2a657e24c2fe6aca6046cd6e94fb9
Martin Schmal <martinschmal@gmail.com>
2020-02-10 16:44:17 +0100

    added demo map view

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const mapboxToken = process.env.MAPBOXTOKEN;
+const Table = require("../models/Table");
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index", { mapboxToken: JSON.stringify(mapboxToken) });
+// router.get("/", (req, res, next) => {
+//   console.log(res);
+//   res.render("index", { mapboxToken: JSON.stringify(mapboxToken) });
+// });
+
+
+
+axios
+.get(`http://localhost:3000/`) // the first argument is the path of the route we registered on the server and the second is the object with the properties that need to be updated: `coordinates` is the name of the field in our Room documents and `coord` is the reference to the variable that holds the updated coordinates of the marker
+.then(() => {
+  console.log("Room updated!");
+})
+.catch(err => {
+  console.log(err);
 });
+
+
 
 module.exports = router;
