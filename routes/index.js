2b2d5dc720886e0476cc7fee646e618afb229c30
Marcel <marcel.riek@googlemail.com>
2020-02-10 12:30:22 +0100

    fixed mapbox

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const mapboxToken = process.env.MAPBOXTOKEN;
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index", { mapboxToken: JSON.stringify(mapboxToken) });
 });
 
 module.exports = router;
