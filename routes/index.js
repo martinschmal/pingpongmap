b09c806eafa60989944bba671dcdc58590001790
Martin Schmal <martinschmal@gmail.com>
2020-02-11 14:46:42 +0100

    map showing berlin tables

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,44 @@ const Table = require("../models/Table");
 //   res.render("index");
 // });
 
+let locations = {};
+
 router.get("/", (req, res, next) => {
   Table.find({})
     .then(result => {
-      console.log("result >>> " + result);
-      res.render("index.hbs", { tablesList: result });
+      console.log("result >>> " + result[0]);
+      const cleanedResults = result.map(table => {
+        return {
+          type: "Feature",
+          properties: {
+            Name: table.description,
+            Address: table.location,
+            ["marker-color"]: "#112993",
+            ["marker-symbol"]: "star",
+            ["marker-size"]: "small"
+          },
+          geometry: table.geoJSON
+        };
+      });
+
+      var tableLocation = {
+        type: "FeatureCollection",
+        features: cleanedResults
+      };
+
+      res.render("index.hbs", {
+        tablesList: result,
+        tableLocation: JSON.stringify(tableLocation)
+      });
+
+      //  tableLocation = {
+      //     "type": "FeatureCollection",
+      //     "features": [
+      //         {
+      //             "type": "Feature",
+      //             "properties": { "description": description , "location": location },
+      //             "geometry": geoJSON
+      //         },
     })
     .catch(err => {
       next(err);
