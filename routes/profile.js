849b5cba2dadf53fda8a19b8c2d741be8b6e30da
Marcel <marcel.riek@googlemail.com>
2020-02-11 16:17:24 +0100

    syncing

--- routes/profile.js
+++ routes/profile.js
@@ -14,7 +14,7 @@ router.get("/profile", (req, res, next) => {
 
 // GET route to display a form to edit the profile
 router.get("/profile/:userId/edit", (req, res, next) => {
-  const promises = [Author.find({}), user.findById(req.body._id)];
+  const promises = [Author.find({}), user.findById(req.params.userId)];
 
   if (!req.session.user) {
     res.redirect("/");
@@ -24,6 +24,23 @@ router.get("/profile/:userId/edit", (req, res, next) => {
   Promise.all(promises)
     .then(results => {
       res.render("profileEdit.hbs");
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// POST route to
+
+// GET route to delete the profile
+router.get("/profile/:userId/delete", (req, res, next) => {
+  if (!req.session.user) {
+    res.redirect("/");
+    return;
+  }
+  User.deleteOne({ _id: req.params.userId })
+    .then(() => {
+      res.redirect("/");
     })
     .catch(err => {
       next(err);
