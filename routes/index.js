bb0e648b37c6cf4c14e6c28274177afabfdf0da9
Martin Schmal <martinschmal@gmail.com>
2020-02-11 10:33:41 +0100

    layout update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Table = require("../models/Table");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+// router.get("/", (req, res, next) => {
+//   res.render("index");
+// });
+
+router.get("/", (req, res, next) => {
+  Table.find({})
+    .then(result => {
+      console.log("result >>> " + result);
+      res.render("index.hbs", result);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
