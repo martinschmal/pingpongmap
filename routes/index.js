04d265c802fa2202ffdecd4824fec4f968b3be7e
Martin Schmal <martin.schmal@hausify.org>
2020-02-13 16:36:02 +0100

    styling

--- routes/index.js
+++ routes/index.js
@@ -2,53 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Table = require("../models/Table");
 
-/* GET home page */
-// router.get("/", (req, res, next) => {
-//   res.render("index");
-// });
-
-let locations = {};
-
-// router.get("/", (req, res, next) => {
-//   Table.find({})
-//     .then(result => {
-//       console.log("result >>> " + result[0]);
-//       const cleanedResults = result.map(table => {
-//         return {
-//           type: "Feature",
-//           data: {
-//             Name: table.description,
-//             Address: table.location,
-//             table_image: table.table_image,
-//             ["marker-color"]: "#112993",
-//             ["marker-symbol"]: "star",
-//             ["marker-size"]: "small",
-//             geometry: table.geometry
-//           },
-
-//         };
-//       });
-
-//       var tableLocation = {
-//         type: "FeatureCollection",
-//         features: cleanedResults
-//       };
-
-//       res.render("index.hbs", {
-//         tablesList: result,
-//         tableLocation: JSON.stringify(tableLocation),
-//         user: req.session.user
-//       });
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// });
-
 router.get("/", (req, res, next) => {
   Table.find({})
     .then(result => {
-      console.log("result >>> " + result[0]);
+      //console.log("result >>> " + result[0]);
       const cleanedResults = result.map(table => {
         return {
           type: "Feature",
@@ -74,7 +31,7 @@ router.get("/", (req, res, next) => {
       //console.log(result);
       res.render("index-map-gl.hbs", {
         tablesList: result,
-        tableTest: JSON.stringify(result),
+        //tableTest: JSON.stringify(result),
         tableLocation: JSON.stringify(tableLocation),
         user: req.session.user
       });
